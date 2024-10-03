07cf12a5602fd85b145830431f1c2ee9073ab42f
marcmax <68873897+marcmax@users.noreply.github.com>
2024-10-03 17:54:17 -0300

    Actualización de migraciones
    se actualizaron las migraciones de la BD y se agregaron las relaciones de las mismas

---
+++ database/migrations/2024_09_06_121850_create_pais_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pais', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre')->unique();
+            $table->integer('medallas_oro')->default(0);
+            $table->integer('medallas_plata')->default(0);
+            $table->integer('medallas_bronce')->default(0);
+            $table->integer('total_medallas')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pais');
+    }
+};
